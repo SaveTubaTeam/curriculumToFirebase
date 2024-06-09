ccbb2aa2fbb40dacd472b8df20e85311c9673c19
James Chang <jameschang2005@icloud.com>
2024-06-09 10:53:06 -0400

    cleanup

--- src/curriculumToJSON/CurriculumForm.jsx
+++ src/curriculumToJSON/CurriculumForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Submit from './Submit.jsx';
+import { downloadBlob, parseDocument } from './parser.js';
 
 function CurriculumForm({ gapiState }) {
    const [value, setValue] = useState("");
@@ -72,5 +72,64 @@ function CurriculumDropdown({ value, handleChange }) {
      </select>
    );
 }
+
+//@param {string} value the Google Document ID selected from the dropdown above
+//@param {Google API Client Object} gapiState
+//@param handleMessage a function to pass the message state back up to CurriculumForm
+function Submit({ value, gapiState, handleMessage }) {
+   //soft disable button
+   const [disabled, setDisabled] = useState(true);
+ 
+   //if the parser is running, we hard disable the submit button
+   const [loading, setLoading] = useState(false);
+ 
+   //checking for valid dropdown entry and gapi initialization
+   useEffect(() => {
+      if (value !== "" && gapiState !== null) {
+         setDisabled(false);
+         console.log("Submit button enabled");
+     } else {
+         setDisabled(true);
+     }
+   }, [value, gapiState])
+ 
+   function handleClick() {
+      if (disabled) {
+         handleMessage("The Submit button is currently disabled. Sign in and select a document to enable the Submit button.");
+      } else {
+         setLoading(true);
+         handleMessage("Loading . . . ");
+         console.log(`Document ID: ${value}`);
+         getDoc();
+      }
+   };
+ 
+   // Function to get document and handle the download
+   async function getDoc() {
+      try {
+         const response = await gapi.client.docs.documents.get({
+            documentId: value,
+         });
+         const doc = response.result;
+         console.log(`"${doc.title}" found.`);
+         
+         const jsonContent = parseDocument(doc); //see parser.js
+         
+         const blob = new Blob([jsonContent], { type: 'application/json' });
+         downloadBlob(blob, doc.title); //see parser.js
+ 
+         handleMessage(`Document "${doc.title}" downloaded as JSON.`);
+         setLoading(false);
+      } catch (err) {
+         console.error(err);
+         handleMessage(`Error: ${err.message}`);
+         setLoading(false);
+      }
+   };
+ 
+   return (
+      <button disabled={loading} onClick={handleClick}>Submit</button>
+   )
+ }
 
 export default CurriculumForm;
