99af53feb7b23440da1ff82d6dcc1030e22f9410
James Chang <jameschang2005@icloud.com>
2024-06-11 15:34:09 -0400

    made some comments to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config(); //to configure dotenv
 
-// https://vitejs.dev/config/
+//env variables: https://vitejs.dev/guide/env-and-mode 
+// Note how .env variables as accessed as import.meta.env.VITE_ENVIRONMENT_VARIABLE
+// Also note how self-defined .env variables are prefixed with VITE_
+
+//config: https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()]
 })
